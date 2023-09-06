55c32a6e016124c0e73473da9db7639fd5b07b7d
bohdan-kharytonov <bohdan.kharytonov@lembergsolutions.com>
2023-09-06 18:40:31 +0300

    Add libraries, implemented basic architecture

---
+++ include/data_manager.h
@@ -0,0 +1,14 @@
+#ifndef __DATA_MANAGER_H__
+#define __DATA_MANAGER_H__
+
+class DataManager
+{
+private:
+
+public:
+    DataManager();
+    
+};
+
+#endif /* __DATA_MANAGER_H__ */
+
---
+++ include/data_output.h
@@ -0,0 +1,17 @@
+#ifndef __DATA_OUTPUT_H__
+#define __DATA_OUTPUT_H__
+
+#include "../include/raspberry_bmp280.h"
+#include "data_reading.h"
+class DataOutput
+{
+private:
+    /* data */
+public:
+    DataOutput(/* args */);
+
+    int print_info();
+};
+
+#endif /* __DATA_OUTPUT_H__ */
+
