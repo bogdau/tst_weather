eb5e8ff883727424b77b43167b1db9cb1d3049e7
bohdan-kharytonov <bohdan.kharytonov@lembergsolutions.com>
2023-09-08 13:01:30 +0300

    feat(mqtt.cpp, mqtt.h): added the ability to connect and output to a local network using MQTT server

--- include/data_manager.h
+++ include/data_manager.h
@@ -7,6 +7,7 @@ class DataManager
 public:
     DataManager() = default;
     int data_manager();
+    void start_mqtt(double temp_cel, double temp_far, double pressure);
 };
 
 #endif /* __DATA_MANAGER_H__ */
--- src/data_manager.cpp
+++ src/data_manager.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <iomanip>
+#include <mosquittopp.h>
 
 #include "data_manager.h"
 #include "data_output.h"
 #include "data_reading.h"
 #include "settings.h"
+#include "mqtt.h"
 
 int DataManager::data_manager(){ 
     BMP280 bmp;
@@ -31,6 +34,7 @@ int DataManager::data_manager(){
             bmp.delay_ms(120);
         }
         dtrd.dataRefresh();
+        start_mqtt(dtrd.getTempCels(),dtrd.getTempFahr(),dtrd.getPressure() );
         dtot.print_info(dtrd.getTempCels(),dtrd.getTempFahr(),dtrd.getPressure() );
 
         i++;
@@ -38,4 +42,49 @@ int DataManager::data_manager(){
     }
 
     return 0;
+}
+
+
+void DataManager::start_mqtt(double temp_cel, double temp_far, double pressure){
+    class mqtt_client *iot_client;
+    int rc;  
+
+    std::stringstream mqtt_string_builder;
+    mqtt_string_builder  << "Temperature: " <<  std::fixed << std::setprecision(2) << temp_cel<<" ℃, "<< temp_far  <<   " ℉ "  << " Pressure: " <<  int(pressure) << " Pa" ;;
+
+    std::string client_id = "user";
+    std::string  host  = "localhost";
+    int port = 1883;
+    std::string MQTT_TOPIC = "Test";
+
+    int mid_val {4};
+	int *mid {&mid_val};
+	std::string  topic_val = "Test";
+	std::string topic = topic_val;
+	std::string payload = mqtt_string_builder.str();
+
+	int qos {1};
+	bool retain {true};
+
+    mosqpp::lib_init();
+
+    iot_client = new mqtt_client(client_id.c_str(), host.c_str(), port);    
+
+
+    rc = iot_client->loop();
+    if (rc)
+    {
+        iot_client->reconnect();
+    }
+    else
+        iot_client->subscribe(NULL, MQTT_TOPIC.c_str());
+
+        int publish_status = iot_client->publish(mid, topic.c_str(), payload.size(), payload.c_str(), qos, retain);
+        if(publish_status != MOSQ_ERR_SUCCESS){
+            std::cout << "Error: unable to publish message to Cumulocity MQTT broker.";
+    }
+
+    
+
+    mosqpp::lib_cleanup();
 }
