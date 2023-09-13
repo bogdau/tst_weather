75f3d936aaf22b688fc3e6c7f47293902f96d890
bohdan-kharytonov <bohdan.kharytonov@lembergsolutions.com>
2023-09-13 14:41:58 +0300

    feat(main.cpp):created a connection between the user interface and the command line

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 #include <memory>
-#include "data_manager.h"
+#include <QWidget>
+#include <QObject>
+#include <QApplication>
 
-int main()
+#include "include/bmp280data.h"
+#include "include/RawData.h"
+#include "include/data_manager.h"
+#include "form.h"
+
+int main(int argc, char *argv[])
 {
-    std::unique_ptr<DataManager> dtmg;
-    dtmg->data_manager();
+    QApplication app(argc, argv);
+
+    std::unique_ptr<DataManager> dtrt = std::make_unique<DataManager>();
+    Form main_window;
+
+    BMP280Data *bmp_data = new BMP280Data;
+    RawData *raw_data = new RawData;
+
+    if(bmp_data->init() ==  true){
+        dtrt->class_manager(raw_data);
+    }
+    else{
+        dtrt->class_manager(bmp_data);
+    }
+
+    QObject::connect(dtrt.get(), &DataManager::tempChange, &main_window, &Form::takeTemp);
+    QObject::connect(dtrt.get(), &DataManager::pressChange, &main_window, &Form::takePressure);
+    main_window.show();
+    dtrt->data_manager();
+
+    return app.exec();
+
 }
