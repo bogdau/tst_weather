075b7cbe3a99a8bc2339f391e5f9ebd1ce47f272
bohdan-kharytonov <bohdan.kharytonov@lembergsolutions.com>
2023-11-13 11:02:23 +0000

    feat(data_base.cpp): fix Qvector index out of range

--- data_base.cpp
+++ data_base.cpp
@@ -78,9 +78,14 @@ void data_base::insert_table_temp_press(int temp, int pressure){
 void data_base::insert_table_magnetometr(QVector<int> x){
     char* errMsg = 0;
     char insertDataSQL[100];
-    snprintf(insertDataSQL, sizeof(insertDataSQL),
+    if(x.size() == 3){
+        snprintf(insertDataSQL, sizeof(insertDataSQL),
              "INSERT INTO MAGNETOMETR_DATA (X, Y, Z) VALUES (%d, %d, %d);",
              x[0],x[1],x[2]);
+    }
+    else{
+        return;
+    }
 
     int rc = sqlite3_exec(db, insertDataSQL, 0, 0, &errMsg);
 
