acadc711579e7cb2bd583c6955a3c61e09e7d6db
bohdan-kharytonov <bohdan.kharytonov@lembergsolutions.com>
2023-11-20 11:51:21 +0000

    ref():removed the commented code

--- BMP280/BMP280.h
+++ BMP280/BMP280.h
@@ -1,19 +1,3 @@
-/*
- * Copyright 2017 Cengis KOCAURLU
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *    http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include <stdint.h>
 #include <stdio.h>
 #include "i2c_impl/I2CDevice.h"
@@ -21,19 +5,17 @@
 #ifndef __I2C_TK_SDK_BMP280_H__
 #define __I2C_TK_SDK_BMP280_H__
 
-//Valid chip ID
 #define __BMP280_CHIP_ID1	0x56
 #define __BMP280_CHIP_ID2	0x57
 #define __BMP280_CHIP_ID3	0x58
 
-//Calibration registers for temperature
 #define __BMP280_REGISTER_CALIBRATION_DIG_TEMPERATURE_T1_LSB		0x88
 #define __BMP280_REGISTER_CALIBRATION_DIG_TEMPERATURE_T1_MSB		0x89
 #define __BMP280_REGISTER_CALIBRATION_DIG_TEMPERATURE_T2_LSB		0x8A
 #define __BMP280_REGISTER_CALIBRATION_DIG_TEMPERATURE_T2_MSB		0x8B
 #define __BMP280_REGISTER_CALIBRATION_DIG_TEMPERATURE_T3_LSB		0x8C
 #define __BMP280_REGISTER_CALIBRATION_DIG_TEMPERATURE_T3_MSB		0x8D
-//Calibration registers for pressure
+
 #define __BMP280_REGISTER_CALIBRATION_DIG_PRESSURE_P1_LSB			0x8E
 #define __BMP280_REGISTER_CALIBRATION_DIG_PRESSURE_P1_MSB			0x8F
 #define __BMP280_REGISTER_CALIBRATION_DIG_PRESSURE_P2_LSB			0x90
@@ -53,8 +35,6 @@
 #define __BMP280_REGISTER_CALIBRATION_DIG_PRESSURE_P9_LSB			0x9E
 #define __BMP280_REGISTER_CALIBRATION_DIG_PRESSURE_P9_MSB			0x9F
 
-
-//Command registers
 #define __BMP280_REGISTER_CHIP_ID					0xD0
 #define __BMP280_REGISTER_RESET						0xE0
 #define __BMP280_REGISTER_STATUS					0xF3
@@ -67,13 +47,10 @@
 #define __BMP280_REGISTER_TEMPERATURE_LSB			0xFB
 #define __BMP280_REGISTER_TEMPERATURE_XLSB			0xFC
 
-//Timing
 #define __BMP280_T_INIT_MAX							20	/* 20/16 = 1.25 	ms */
 #define __BMP280_T_MEASURE_PER_OSRS_MAX				37	/* 37/16 = 2.3125	ms */
 #define __BMP280_T_SETUP_PRESSURE_MAX				10	/* 10/16 = 0.625	ms */
 
-
-//Predefined values
 #define __BMP280_CONST_COMPLETE_RESET				0xB6
 
 typedef struct __BMP280_CALIBRATION {
@@ -136,8 +113,6 @@ public:
 	virtual ~BMP280();
 
 	int initialize();
-
-	//BMP280_VALUES read();
 
 	uint8_t getChipId(){ return this->_chipId;};
 
