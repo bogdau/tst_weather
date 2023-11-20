52caafe9d8c5162ade846bd232d36bbdff300d0c
bohdan-kharytonov <bohdan.kharytonov@lembergsolutions.com>
2023-11-20 11:48:43 +0000

    ref():removed the commented code

--- BMP280/BMP280.cpp
+++ BMP280/BMP280.cpp
@@ -28,11 +28,6 @@ int BMP280::initialize(){
 	return 0;
 }
 
-/*
- * Register 0xD0 “id”
- * The “id” register contains the chip identification number chip_id[7:0], which is 0x58. 
- * This number can be read as soon as the device finished the power-on-reset.
- */
 uint8_t BMP280::readChipId()
 {
 	uint8_t buffer[1] = {__BMP280_REGISTER_CHIP_ID};
@@ -41,12 +36,7 @@ uint8_t BMP280::readChipId()
 	I2CDevice::read(buffer,1);
 	return buffer[0];
 }
-/*
- * Register 0xE0 “reset”
- * The “reset” register contains the soft reset word reset[7:0]. If the value 0xB6 is written to the
- * register, the device is reset using the complete power-on-reset procedure. Writing other values
- * than 0xB6 has no effect. The readout value is always 0x00.
- */
+
 void BMP280::reset(bool completeReset)
 {
 	uint8_t buffer[2] = {__BMP280_REGISTER_RESET, static_cast<uint8_t>(completeReset ? __BMP280_CONST_COMPLETE_RESET : 0x00)};
@@ -58,22 +48,6 @@ void BMP280::reset(bool completeReset)
 	this->_operatingMode = SLEEP;
 }
 
-/*
- * Register 0xF3 “status”
- * The “status” register contains two bits which indicate the status of the device.
- *
- * Register 0xF3	Name 			Description
- * “status”
- * 
- * Bit 3 			measuring[0] 	Automatically set to ‘1’ whenever a conversion is running 
- * 									and back to ‘0’ when the results have been transferred 
- * 									to the data registers.
- * 								
- * Bit 0 			im_update[0] 	Automatically set to ‘1’ when the NVM data are being
- * 									copied to image registers and back to ‘0’ when the
- * 									copying is done. The data are copied at power-on-reset
- * 									and before every conversion.
- */
 BMP280_STATUS BMP280::status()
 {
 	uint8_t buffer[1] = {__BMP280_REGISTER_STATUS};
@@ -86,18 +60,6 @@ BMP280_STATUS BMP280::status()
 	status.imUpdate 	= (buffer[0] & 0x01) > 0;
 	return status;
 }
-
-/*
- * Register 0xF4 “ctrl_meas”
- * The “ctrl_meas” register sets the data acquisition options of the device.
- * Table 20: Register 0xF4 “ctrl_meas”
- * 
- * Register 0xF4		Name 			Description
- * “ctrl_meas”
- * Bit 7, 6, 5 			osrs_t[2:0] 	Controls oversampling of temperature data. See chapter 3.3.2 for details.
- * Bit 4, 3, 2 			osrs_p[2:0] 	Controls oversampling of pressure data. See chapter 3.3.1 for details.
- * Bit 1, 0 			mode[1:0] 		Controls the power mode of the device. See chapter 3.6 for details.
- */
 
 void BMP280::controlMeasure(BMP280_OVERSAMPLING pressureOversampling, BMP280_OVERSAMPLING temperatureOversampling, BMP280_OPERATING_MODE operatingMode)
 {
@@ -114,28 +76,11 @@ void BMP280::controlMeasure(BMP280_OVERSAMPLING pressureOversampling, BMP280_OVE
 	I2CDevice::write(buffer,2);
 }
 
-/*
- * Register 0xF5 “config”
- * The “config” register sets the rate, filter and interface options of the device. Writes to the “config”
- * register in normal mode may be ignored. In sleep mode writes are not ignored.
- * 
- * Register 0xF5		Name 				Description
- * “config”
- * Bit 7, 6, 5 			t_sb[2:0] 			Controls inactive duration tstandby in normal mode. See chapter 3.6.3 for details.
- * Bit 4, 3, 2 			filter[2:0] 		Controls the time constant of the IIR filter. See chapter 3.3.3 for details.
- * Bit 0 				spi3w_en[0] 		Enables 3-wire SPI interface when set to ‘1’. See chapter 5.3 for details.
- */
 void BMP280::config()
 {
-	//TODO Implementation
+
 }
 
-/*
- * Register 0xF7…0xF9 “press” (_msb, _lsb, _xlsb)
- * The “press” register contains the raw pressure measurement output data up[19:0]. For details
- * on how to read out the pressure and temperature information from the device, please consult
- * chapter3.9.
- */
 double BMP280::pressure()
 {
 	uint8_t reg[1] = {__BMP280_REGISTER_PRESSURE_MSB};
@@ -171,12 +116,6 @@ double BMP280::compensatePressure(int32_t rawValue){
  	return pressure;
 }
 
-/*
- * Register 0xFA…0xFC “temp” (_msb, _lsb, _xlsb)
- * The “temp” register contains the raw temperature measurement output data ut[19:0]. For details
- * on how to read out the pressure and temperature information from the device, please consult
- * chapter 3.9.
- */
 double BMP280::temperature()
 {
 	uint8_t reg[1] = {__BMP280_REGISTER_TEMPERATURE_MSB};
@@ -226,17 +165,6 @@ void BMP280::forceReadTemperaturePresure(double *temperature, double *pressure)
 	}
 }
 
-/*
- * 	Trimming parameter readout
- * 	The trimming parameters are programmed into the devices’ non-volatile memory (NVM) during
- * 	production and cannot be altered by the customer. Each compensation word is a 16-bit signed
- * 	or unsigned integer value stored in two’s complement. As the memory is organized into 8-bit
- * 	words, two words must always be combined in order to represent the compensation word. The
- * 	8-bit registers are named calib00…calib25 and are stored at memory addresses 0x88…0xA1.
- * 	The corresponding compensation words are named dig_T# for temperature compensation
- * 	related values and dig_P# for pressure compensation related values. The mapping is shown in
- * 	Table 17.
- */
 BMP280_CALIBRATION BMP280::readCalibration()
 {
 	uint8_t buffer[1] = {	__BMP280_REGISTER_CALIBRATION_DIG_TEMPERATURE_T1_LSB};
